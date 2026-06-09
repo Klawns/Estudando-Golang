05d3761c667745a833f623a1232e575d07a834dc
Klawns <fabriciioklaus@gmail.com>
2026-06-09 15:51:24 -0300

    Feito questão 7 - lampada.go

--- medio/lampada.go
+++ medio/lampada.go
@@ -1,24 +1,41 @@
 package medio
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 type Lampada struct {
-	isLigada bool
-	potencia float64
-	voltagem float64
+	isLigada   bool
+	potencia   float64
+	voltagem   float64
+	isQueimada bool
 }
 
-func NewLampada(isLigada bool, potencia, voltagem float64) *Lampada {
+func NewLampada(isLigada, queimada bool, potencia, voltagem float64) *Lampada {
 	return &Lampada{
-		isLigada: isLigada,
-		potencia: potencia,
-		voltagem: voltagem,
+		isLigada:   isLigada,
+		potencia:   potencia,
+		voltagem:   voltagem,
+		isQueimada: queimada,
 	}
 }
 
 func (l *Lampada) Ligar() {
+	if l.isQueimada {
+		fmt.Println("A lâmpada está queimada e não pode ser ligada.")
+		return
+	}
+
 	if l.isLigada {
 		fmt.Println("A lâmpada já está ligada.")
+		return
+	}
+
+	if rand.Intn(100) < 15 {
+		l.isQueimada = true
+		l.isLigada = false
+		fmt.Println("A lâmpada queimou ao tentar ligar.")
 		return
 	}
 
@@ -27,6 +44,11 @@ func (l *Lampada) Ligar() {
 }
 
 func (l *Lampada) Desligar() {
+	if l.isQueimada {
+		fmt.Println("A lâmpada está queimada.")
+		return
+	}
+
 	if !l.isLigada {
 		fmt.Println("A lâmpada já está desligada.")
 		return
@@ -61,6 +83,7 @@ func (l *Lampada) Display() {
 		fmt.Println("Estado: desligada")
 	}
 
+	fmt.Println("Queimada:", l.isQueimada)
 	fmt.Println("Voltagem:", l.voltagem)
 	fmt.Println("Potência:", l.potencia)
 }
